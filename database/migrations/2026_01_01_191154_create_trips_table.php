8fe3aa001696383c2f210709e7619e4c4ecea68a
Cade Auto Peças <suporte@techvanguard.com.br>
2026-01-04 16:45:27 -0300

    coluna description

--- database/migrations/2026_01_01_191154_create_trips_table.php
+++ database/migrations/2026_01_01_191154_create_trips_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('trips', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('description')->nullable();
             $table->string('invite_code', 10)->unique();
             $table->string('currency', 3)->default('BRL');
             $table->date('start_date');
